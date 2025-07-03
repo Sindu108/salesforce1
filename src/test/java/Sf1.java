75355f666765f48fc7401d4d0796e4edc93ff109
Sindu108 <sindujaprem@gmail.com>
2025-07-03 00:36:21 -0400

    Version 2

--- src/test/java/Sf1.java
+++ src/test/java/Sf1.java
@@ -1,4 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Sf1 {
 
+    @Test
+    public void launchSite(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://orgfarm-d4886ca863-dev-ed.develop.my.salesforce.com/");
+    }
+
+    @Test
+    public void someMethod(){
+        System.out.println("Hellooooo");
+    }
 
 }
